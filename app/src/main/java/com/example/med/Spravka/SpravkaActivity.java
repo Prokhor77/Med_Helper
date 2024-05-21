369ee78b56d3bbfda4ae9c8b0fc974ccc65ca38c
Prokhor77 <prokhorodinets@gmail.com>
2024-05-21 23:51:00 +0300

    5 commit at night

--- app/src/main/java/com/example/med/Spravka/SpravkaActivity.java
+++ app/src/main/java/com/example/med/Spravka/SpravkaActivity.java
@@ -7,20 +7,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.med.R;
+import com.example.med.User.CallsFragmentUser;
+import com.example.med.User.ProfileFragmentUser;
+import com.example.med.User.SpravkaFragmentUser;
+import com.example.med.databinding.ActivitySpravkaBinding;
+import com.example.med.databinding.ActivityUserBinding;
 
 public class SpravkaActivity extends AppCompatActivity {
+
+    ActivitySpravkaBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_spravka);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivitySpravkaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new ProfileFragmentSpravka());
+
+        binding.bottomNavigationViewSpravka.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.profile_spravka) {
+                replaceFragment(new ProfileFragmentSpravka());
+
+            } else if (item.getItemId() == R.id.search_spravka) {
+                replaceFragment(new CallsFragmentSpravka());
+
+            } else if (item.getItemId() == R.id.spravka_spravka) {
+                replaceFragment(new SpravkaFragmentSpravka());
+
+            }
+            return true;
         });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_spravka,fragment);
+        fragmentTransaction.commit();
+
     }
 }
